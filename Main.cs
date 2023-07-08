694a82adad5ed734b82742a85745ade35d793954
qwerty9118 <williamdbmayberry@gmail.com>
2023-07-08 14:23:18 +0100

    More food texturing

--- Main.cs
+++ Main.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
 
-    public string[] foods;
+    public string[] foods =
+    {
+        "Bacon",
+        "Beef",
+        "Bread",
+        "Chicken",
+        "Cucumber",
+        "Egg",
+        "Leek",
+        "Lettuce",
+        "Onion",
+        "Pasta",
+        "Peas",
+        "Pepper",
+        "Potato",
+        "Rice",
+        "Tofu",
+        "Tomato",
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        Resources.LoadAll
 
-        DirectoryInfo dir = new DirectoryInfo("Assets/Kazb0/Resources");
+        //Resources.LoadAll("Food");
+
+        /*DirectoryInfo dir = new DirectoryInfo("Assets/Kazb0/Resources");
         FileInfo[] info = dir.GetFiles("*.prefab");
-        foods = info.Select(f => f.FullName).ToArray();
-        */
+        foods = info.Select(f => f.FullName).ToArray();*/
+        
     }
 
     // Update is called once per frame
--- qwerty9118/FoodItem.cs
+++ qwerty9118/FoodItem.cs
@@ -9,6 +9,9 @@ public class FoodItem : MonoBehaviour
     [SerializeField] int cookedLevel = 0;
     private int oldCutLevel = 0;
     private int oldCookedLevel = 0;
+
+    private Sprite[,] foodStateTextures = new Sprite[3, 2];
+
     public PlayerMovement movement;
     public bool isMoving{get{return movement.enabled;}set{}}//used by: SandwichItemHandler
     public bool isSelected;
@@ -18,10 +21,57 @@ public class FoodItem : MonoBehaviour
     {
 
         movement = GetComponent<PlayerMovement>();
-        //.Log("Food/" + this.gameObject.name + cutLevel + cookedLevel);
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load(
-            "Food/" + this.gameObject.name + cutLevel + cookedLevel,
+
+        this.foodStateTextures[0,0] = Resources.Load(
+            "Food/" + this.gameObject.name + "00",
             typeof(Sprite)) as Sprite;
+        this.foodStateTextures[1, 0] = Resources.Load(
+            "Food/" + this.gameObject.name + "10",
+            typeof(Sprite)) as Sprite;
+        this.foodStateTextures[2, 0] = Resources.Load(
+            "Food/" + this.gameObject.name + "20",
+            typeof(Sprite)) as Sprite;
+        this.foodStateTextures[0, 1] = Resources.Load(
+            "Food/" + this.gameObject.name + "01",
+            typeof(Sprite)) as Sprite;
+        this.foodStateTextures[1, 1] = Resources.Load(
+            "Food/" + this.gameObject.name + "11",
+            typeof(Sprite)) as Sprite;
+        this.foodStateTextures[2, 1] = Resources.Load(
+            "Food/" + this.gameObject.name + "21",
+            typeof(Sprite)) as Sprite;
+
+        for (int cut = 0; cut < 3; cut++)
+        {
+            for (int cook = 0; cook < 2; cook++)
+            {
+
+                this.foodStateTextures[cut, cook] = Resources.Load(
+                    "Food/" + this.gameObject.name + cut + cook,
+                    typeof(Sprite)) as Sprite;
+
+                if (this.foodStateTextures[cut, cook] == null)
+                {
+
+                    if (cut + cook == 0)
+                    {
+                        this.foodStateTextures[cut, cook] = Resources.Load(
+                            "Food/GenericFoodItem00",
+                            typeof(Sprite)) as Sprite;
+                    }
+
+                    else if (cook == 0)
+                    {
+
+                    }
+
+                }
+
+            }
+        }
+
+        //.Log("Food/" + this.gameObject.name + cutLevel + cookedLevel);
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = ;
 
     }
 
@@ -32,9 +82,7 @@ public class FoodItem : MonoBehaviour
         if (oldCutLevel != cutLevel || oldCookedLevel != cookedLevel)
         {
 
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load(
-                "Food/" + this.gameObject.name + cutLevel + cookedLevel,
-                typeof(Sprite)) as Sprite;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = this.foodStateTextures[cutLevel, cookedLevel];
 
             oldCutLevel = cutLevel;
             oldCookedLevel = cookedLevel;
