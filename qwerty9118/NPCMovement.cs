227fcdda86e861e706f831e1eb38859eef69e3c4
qwerty9118 <williamdbmayberry@gmail.com>
2023-07-08 21:47:50 +0100

    Food NPC spawning & following trails :)))))))

--- qwerty9118/NPCMovement.cs
+++ qwerty9118/NPCMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
+
+    public string parentName = "";
+    private int nextPoint = 0;
+    private int framesForTrail = 15;
+    private int framesTravelled = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,24 @@ public class NPCMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //if parent fooditem is moving, move
+        if (Vector2.Distance(GameObject.Find(parentName).transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().lastPos) > 0)
+        {
+            float distToNext = Vector2.Distance(transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint]);
+
+            framesTravelled++;
+
+            if (framesTravelled > framesForTrail)
+            {
+                framesTravelled = 0;
+                nextPoint++;
+            }
+
+            transform.position += (GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint] - transform.position) / framesForTrail;
+
+        }
+
     }
+
 }
