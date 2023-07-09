07612f86c22be5049ac7638641454bac1d07cb3a
racon <46864506+RaconJS@users.noreply.github.com>
2023-07-09 16:09:10 +0100

    adds knifes

--- RaconJS/KnifeHandler.cs
+++ RaconJS/KnifeHandler.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class KnifeHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start(){
+		
+	}
+	void onTriggerEnter2D(Collider2D collider){
+		var foodItem = collider.gameObject.GetComponent<FoodItem>();
+		foodItem.cutLevel++;
+	}
+	// Update is called once per frame
+	void Update(){
+		
+	}
 }
