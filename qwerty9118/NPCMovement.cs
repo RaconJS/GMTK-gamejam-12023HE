66607b744ffdc778941aef3906010bfc74dc2bf0
qwerty9118 <williamdbmayberry@gmail.com>
2023-07-09 12:14:29 +0100

    following stuff

--- qwerty9118/NPCMovement.cs
+++ qwerty9118/NPCMovement.cs
@@ -9,31 +9,47 @@ public class NPCMovement : MonoBehaviour
     private int nextPoint = 0;
     private int framesForTrail = 15;
     private int framesTravelled = 0;
+    private Vector3 step;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        step = (GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint] - transform.position) / framesForTrail;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //if parent fooditem is moving, move
-        if (Vector2.Distance(GameObject.Find(parentName).transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().lastPos) > 0)
+        //if parent fooditem is moving, or parent fooditem has moved and switched control, move
+        if
+        (
+            (
+                GameObject.Find(parentName).GetComponent<SpawnTrail>().hasMoved && !GameObject.Find(parentName).GetComponent<SpawnTrail>().CheckTrail()
+            )
+
+            || Vector2.Distance(GameObject.Find(parentName).transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().lastPos) > 0
+        )
         {
-            float distToNext = Vector2.Distance(transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint]);
+            //float distToNext = Vector2.Distance(transform.position, GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint]);
 
             framesTravelled++;
 
             if (framesTravelled > framesForTrail)
             {
+
                 framesTravelled = 0;
                 nextPoint++;
+                if (nextPoint <= GameObject.Find(parentName).GetComponent<SpawnTrail>().trail.Count)
+                {
+                    step = (GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint] - transform.position) / framesForTrail;
+                }
+                
             }
 
-            transform.position += (GameObject.Find(parentName).GetComponent<SpawnTrail>().trail[nextPoint] - transform.position) / framesForTrail;
+            transform.position += step;
 
         }
 
